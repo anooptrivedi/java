c7bd7e38746a76015c89b5525f3b08d4560be1a8
anooptrivedi <anoop4@gmail.com>
2020-06-23 13:51:04 -0700

    Add files via upload

---
+++ PrimeNumberGeneratorVersion2.java
@@ -0,0 +1,32 @@
+package ps;
+
+import java.util.Scanner;
+
+public class PrimeNumberGeneratorVersion2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter integer number up to find prime, e.g. 100 : ");
+		
+		int total = sc.nextInt(); //input
+		String listOfPrimeNumbers = ""; //store found prime numbers as a String
+		
+		for(int i = 2; i <= total; i++) { //start with 2 to total
+			
+			boolean isPrime = true;
+			
+			for(int check = 2; check < i; check++) {
+				if(i % check == 0) {
+					isPrime = false;
+				}
+			}
+			if(isPrime) {
+				listOfPrimeNumbers = listOfPrimeNumbers + i + " ";
+			}
+			
+		}
+		System.out.println("Prime number from 1 to " + total + " are : " + listOfPrimeNumbers + "\n");
+		System.exit(0);
+	}
+}
