42ea71ad075a916feb9b005cda5f5cec2b1628dc
anooptrivedi <anoop4@gmail.com>
2020-06-23 13:51:31 -0700

    Add files via upload

---
+++ PrimeNumberGeneratorVersion3.java
@@ -0,0 +1,31 @@
+package ps;
+
+import java.util.Scanner;
+
+public class PrimeNumberGeneratorVersion3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter integer number up to find prime, e.g. 100 : ");
+		int max = sc.nextInt(); //input
+
+		for(int i =2; i < max; i++) {
+			if(isPrime(i)) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+	
+	// iterate only over odd numbers, because all multiples of 2, except 2 is composite
+	// eliminate i*i ; 2 * 2 product will not be prime, same for 3*3 and so forth
+	public static boolean isPrime(int n) {
+		
+		for(int i=2; i * i <= n; i++) {
+			if( n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
