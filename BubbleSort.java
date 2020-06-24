66bb15e195b90b7b9ede42481b4c0fcb8fbac087
anooptrivedi <anoop4@gmail.com>
2020-06-24 16:08:00 -0700

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -38,7 +38,7 @@ public class BubbleSort {
 	}
 	
 	public static void print(int[] array) {
-		for(int i = 0; i < array.length-1; i++) {
+		for(int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
 	}
